be63f95c2802470635e1ce75183de69c676df64a
centos7 <kongyt@localhost.localdomain>
2016-11-15 04:10:05 -0800

    添加测试代码

---
+++ src/msg/test.py
@@ -0,0 +1,26 @@
+#coding: utf-8
+
+from message_pb2 import *
+from socket import *
+from ctypes import *
+import struct
+
+sock = socket(AF_INET, SOCK_STREAM, 0)
+sock.connect(("192.168.31.128", 8888))
+
+req = Request()
+req.login.username = "kongyatong"
+req.login.password = "password"
+req_data = req.SerializeToString()
+datalen = len(req_data)
+msgid = Login_Request
+buf = create_string_buffer(8)
+print msgid
+struct.pack_into('!I', buf, 0, msgid)
+struct.pack_into('!I', buf, 4, datalen)
+
+send_data = buf.raw + req_data
+sock.send(send_data)
+
+data = sock.recv(1024)
+print data
